owenwu811/pytorch

commit a467a4286baebf5871082c4c308d686f6811819f

[inductor] Add shape checks to ExpandView (#113839)
Currently `ExpandView` doesn't check that the expanded shape is valid which may
allow bugs to slip through which cause silent correctness issues.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/113839
Approved by: https://github.com/ezyang

torch/_inductor/ir.py
@@ -1839,6 +1839,15 @@ class ExpandView(BaseView):
             if new_size[i] == -1:
                 assert old_size[i] is not None
                 new_size[i] = old_size[i]
+            elif old_size[i] is None or old_size[i] == 1:
+                pass
+            else:
+                # Expect broadcast compatibility
+                new_size[i] = V.graph.sizevars.expect_equals(
+                    new_size[i],
+                    old_size[i],
+                    msg=f"Broadcast failed in ExpandView({x.get_size()}, {new_size}) on dimension {i}",
+                )
         return new_size
 
     @classmethod

torch/_inductor/sizevars.py
@@ -328,6 +328,21 @@ class SizeVarAllocator:
     def guard_lt(self, left: Expr, right: Expr) -> None:
         assert self.shape_env.evaluate_expr(sympy.Lt(left, right))
 
+    def expect_true(self, expr: Expr, *, msg: str) -> None:
+        expr = sympy_subs(expr, self.inv_precomputed_replacements)
+        self.shape_env.defer_runtime_assert(expr, msg, fx_node=V.graph.current_node)
+
+    def expect_equals(self, left: Expr, right: Expr, *, msg: str) -> Expr:
+        # Prefer returning the expression without unbacked symints
+        if self.shape_env.is_unbacked_symint(left):
+            self.expect_true(sympy.Eq(left, right), msg=msg)
+            return right
+        elif self.shape_env.is_unbacked_symint(right):
+            self.expect_true(sympy.Eq(left, right), msg=msg)
+            return left
+        else:
+            return self.guard_equals(left, right)
+
     # The evaluate functions evaluate some symbolic sympy expression
     # (NB: not necessarily an Expr) and return what the concrete result
     # is, guarding on the expression being that result

test/inductor/test_unbacked_symints.py
@@ -4,6 +4,7 @@ import torch
 
 from torch._dynamo import config as dynamo_config
 from torch._inductor import config as inductor_config
+from torch.testing import make_tensor
 
 from torch.testing._internal.common_utils import IS_LINUX, TestCase as TorchTestCase
 from torch.testing._internal.inductor_utils import HAS_CUDA
@@ -29,6 +30,16 @@ class TestUnbackedSymints(TorchTestCase):
             expected = fn(*example_inputs)
 
         torch.testing.assert_close(actual, expected)
+
+    def test_expand_mismatch(self):
+        def fn(x):
+            nz = x.nonzero()
+            return nz.expand([-1, 128])
+
+        x = make_tensor(32, 4, device="cpu", dtype=torch.float32, exclude_zero=True)
+        with dynamo_config.patch({"capture_dynamic_output_shape_ops": True}):
+            with self.assertRaises(torch._dynamo.exc.TorchRuntimeError):
+                actual = torch.compile(fn, fullgraph=True)(x)
 
     def test_autotuning(self):
         def fn(x, y):

commit 1d96f5ae1fe07a6f1835c3e04dcd32fc59ad93d1

Remove API that allows for extra deferred runtime asserts during lowering
I want to generate runtime assert nodes during lowering, which means
that I need a finalized list of asserts by the time I start lowering.
This means this runtime assert introduced in
https://github.com/pytorch/pytorch/pull/113839 must go.  Fortunately,
this runtime assert was never exercisable, apparently, and the test
still "passes" without it.  I replace it with a compile time test.  We
can revisit if this assert fails in practice.

Signed-off-by: Edward Z. Yang <ezyang@meta.com>

ghstack-source-id: 4954f815a31cd8c188057a5eb8116acd20ff8f02
Pull Request resolved: https://github.com/pytorch/pytorch/pull/124864

torch/_inductor/ir.py
@@ -1949,11 +1949,9 @@ class ExpandView(BaseView):
                 pass
             else:
                 # Expect broadcast compatibility
-                new_size[i] = V.graph.sizevars.expect_equals(
-                    new_size[i],
-                    old_size[i],
-                    msg=f"Broadcast failed in ExpandView({x.get_size()}, {new_size}) on dimension {i}",
-                )
+                assert V.graph.sizevars.statically_known_equals(
+                    new_size[i], old_size[i]
+                ), f"{new_size[i]} != {old_size[i]}"
         return new_size
 
     @classmethod

torch/_inductor/sizevars.py
@@ -328,21 +328,6 @@ class SizeVarAllocator:
     def guard_lt(self, left: Expr, right: Expr) -> None:
         assert self.shape_env.evaluate_expr(sympy.Lt(left, right))
 
-    def expect_true(self, expr: Expr, *, msg: str) -> None:
-        expr = sympy_subs(expr, self.inv_precomputed_replacements)  # type: ignore[arg-type]
-        self.shape_env.defer_runtime_assert(expr, msg, fx_node=None)
-
-    def expect_equals(self, left: Expr, right: Expr, *, msg: str) -> Expr:
-        # Prefer returning the expression without unbacked symints
-        if self.shape_env.is_unbacked_symint(left):
-            self.expect_true(sympy.Eq(left, right), msg=msg)  # type: ignore[arg-type]
-            return right
-        elif self.shape_env.is_unbacked_symint(right):
-            self.expect_true(sympy.Eq(left, right), msg=msg)  # type: ignore[arg-type]
-            return left
-        else:
-            return self.guard_equals(left, right)
-
     def guarded_order(self, seq):
         """
         Return the order of a sequence as a permutation of range(len(seq)) and guard on that order not changing.

test/inductor/test_unbacked_symints.py
@@ -38,14 +38,14 @@ class TestUnbackedSymints(InductorTestCase):
 
     @skipCUDAIf(not HAS_CUDA, "requires cuda")
     @dynamo_config.patch({"capture_dynamic_output_shape_ops": True})
-    def test_expand_mismatch(self, device):
+    def test_expand_ok_with_runtime_assert(self, device):
         def fn(x):
             nz = x.nonzero()
-            return nz.expand([-1, 128])
+            torch._check(nz.size(0) == 128)
+            return nz.expand([128, -1, 2])
 
         x = make_tensor(32, 4, device=device, dtype=torch.float32, exclude_zero=True)
-        with self.assertRaises(torch._dynamo.exc.TorchRuntimeError):
-            actual = torch.compile(fn, fullgraph=True)(x)
+        actual = torch.compile(fn, fullgraph=True)(x)
 
     @skipCUDAIf(not HAS_CUDA, "requires cuda")
     @dynamo_config.patch({"capture_dynamic_output_shape_ops": True})